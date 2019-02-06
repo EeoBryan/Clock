64a7c53e32281d5a89d22e3c93dd724a6a481640
Bryan Eeo <sadboys.gonna.sad@gmail.com>
2019-02-06 19:18:16 +0800

    Update sketch.js

--- p5/p5clock/sketch.js
+++ p5/p5clock/sketch.js
@@ -1,15 +1,56 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500, 500);
+  angleMode(DEGREES);
 }
 
 function draw() {
   background(0);
+  translate(200, 200);
+  rotate(-90);
 
   let hr = hour();
   let mn = minute();
   let sc = second();
 
+  strokeWeight(8);
+  stroke(255, 100, 150);
+  noFill();
+  let secAngle =  map(sc, 0, 60, 0, 360);
+  arc(0, 0, 300, 300, 0, secAngle);
+
+  stroke(150, 100, 255);
+  let minAngle =  map(mn, 0, 60, 0, 360);
+  arc(0, 0, 280, 280, 0, minAngle);
+
+  stroke(150, 255, 100);
+  let hrAngle =  map(hr % 12, 0, 12, 0, 360);
+  arc(0, 0, 260, 260, 0, hrAngle);
+
+  push();
+  rotate(secAngle);
+  stroke(255, 100, 150);
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(minAngle);
+  stroke(150, 100, 255);
+  line(0, 0, 75, 0);
+  pop();
+
+  push();
+  rotate(hrAngle);
+  stroke(150, 255, 100);
+  line(0, 0, 50, 0);
+  pop();
+
   fill(255);
   noStroke();
-  
+  rotate(90);
+  textSize(20);
+  text(hr + ':' + mn + ':' + sc, 10, 200);
+
+  stroke(255);
+  point(0,0);
 }
+
